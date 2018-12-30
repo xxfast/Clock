e7b45c2c88cdf2842a9b1781e074dc60e3b01172
xxfast <isuru@xfastgames.com>
2018-12-30 13:33:19 +0530

    Initial Commit

---
+++ Clock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clock
+//
+//  Created by Isuru Rajapakse on 30/12/18.
+//  Copyright © 2018 Isuru Rajapakse. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
